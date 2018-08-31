2095520fd26d547ac6ca9f76ee02ca9ab2bc1c23
seonghee2 <42488774+seonghee2@users.noreply.github.com>
2018-09-01 00:34:51 +0900

    declare class BLOCK

--- block.py
+++ block.py
@@ -1,16 +1,4 @@
 
-# block class
-# Don't change block value!!
+# BLOCK class
 class BLOCK:
-    CONST_2 = 2
-    CONST_4 = 4
-    CONST_8 = 8
-    CONST_16 = 16
-    CONST_32 = 32
-    CONST_64 = 64
-    CONST_128 = 128
-    CONST_256 = 256
-    CONST_512 = 512
-    CONST_1024 = 1024
-    CONST_2048 = 2048
-
+    block = 0   # block value
