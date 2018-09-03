c7fc1c3034088bb5a00a1bc16279aeb55ca18239
fpdjsns.github.io <withham1@naver.com>
2018-09-03 19:32:59 +0900

    add blcok fill color

--- block.py
+++ block.py
@@ -1,4 +1,30 @@
+from PyQt5.QtGui import QColor
 
 # BLOCK class
 class BLOCK:
     value = 0   # block value
+
+# BLOCK array class
+class BLOCK_ARRAY:
+
+    # set BLOCK array 4*4
+    blocks = [[BLOCK()]*4 for i in range(4)]
+
+class COLOR:
+
+    FILLS = {}
+
+    def __init__(self):
+
+        self.FILLS[0] = QColor(255, 255, 255)
+        self.FILLS[2] = QColor(253, 247, 234)
+        self.FILLS[4] = QColor(246, 223, 170)
+        self.FILLS[8] = QColor(237, 190, 86)
+        self.FILLS[16] = QColor(255, 102, 0)
+        self.FILLS[32] = QColor(253, 69, 16)
+        self.FILLS[64] = QColor(253, 28, 16)
+        self.FILLS[128] = QColor(137, 199, 236)
+        self.FILLS[256] = QColor(65, 168, 223)
+        self.FILLS[512] = QColor(42, 142, 223)
+        self.FILLS[1024] = QColor(27, 86, 184)
+        self.FILLS[2048] = QColor(62, 67, 135)
--- drawBlock.py
+++ drawBlock.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPainter
+from PyQt5.QtGui import QPainter, QFont
 from PyQt5.QtCore import Qt
+from block import BLOCK_ARRAY, COLOR
+from PyQt5.QtGui import QColor
 
 class Game2048(QMainWindow):
 
@@ -9,11 +11,15 @@ class Game2048(QMainWindow):
     WINDOW_WIDTH = 1200
     WINDOW_HEIGHT = 800
 
+    blockArray = BLOCK_ARRAY()
+
     def __init__(self):
         super().__init__()
         # set window
         self.setGeometry(self.WINDOW_LEFT, self.WINDOW_TOP, self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
         self.setWindowTitle('2048')
+        # set setting
+        self.initGameSetting()
         # set game screen
         self.initGameScreen()
         # show UI
@@ -32,11 +38,14 @@ class Game2048(QMainWindow):
         self.drawBlock.move(50, self.WINDOW_HEIGHT / 2 - self.drawBlock.BLOCK_SIZE*2)
         self.drawBlock.resize(self.drawBlock.BLOCK_SIZE*4, self.drawBlock.BLOCK_SIZE*4)
 
+    def initGameSetting(self):
+        self.blockArray = BLOCK_ARRAY()
 
-class DrawBlock(QWidget):
+class DrawBlock(QWidget, BLOCK_ARRAY):
 
     # one block size (150 x 150)
     BLOCK_SIZE = 150
+    COLOR = COLOR()
 
     def paintEvent(self, event):
         qp = QPainter(self)
@@ -47,7 +56,20 @@ class DrawBlock(QWidget):
         # fill color
         qp.setBrush(Qt.darkRed)
 
+        # set font
+        # font = QFont("test")
+        # font.setPointSize(12)
+        # qp.setFont(font)
+        # qp.drawText("test")
+
+        ind = 1
+
         # draw 4x4 block rectangle
         for i in range(0, 4):
             for j in range(0, 4):
-                qp.drawRect(self.BLOCK_SIZE*i, self.BLOCK_SIZE*j, self.BLOCK_SIZE, self.BLOCK_SIZE)
+                if ind >= 2048:
+                    ind = 1
+                ind *= 2
+                self.blocks[j][i].value = ind
+                qp.setBrush(self.COLOR.FILLS[self.blocks[j][i].value])
+                qp.drawRect(self.BLOCK_SIZE*j, self.BLOCK_SIZE*i, self.BLOCK_SIZE, self.BLOCK_SIZE)
