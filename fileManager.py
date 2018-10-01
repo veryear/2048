9d999e07992efe4008102f3a6108d966cb5f94f6
fpdjsns.github.io <withham1@naver.com>
2018-10-01 21:03:03 +0900

    modify fileManager.py add fileDirect

--- fileManager.py
+++ fileManager.py
@@ -1,15 +1,20 @@
 import codecs
+import os
 # txt 파일 한정
 class FileManager:
 
-    def writeFile(self, fileName, content):
-        f = codecs.open(fileName, 'w', encoding='utf8')
+    def writeFile(self, fileDirect, fileName, content):
+        if not os.path.exists(fileDirect):
+            os.makedirs(fileDirect)
+        f = codecs.open(fileDirect + fileName, 'w', encoding='utf8')
         f.write(content)
         f.close()
 
     # 한 줄만 읽음.
-    def readLineFile(self, fileName):
-        f = codecs.open(fileName, 'r', encoding='utf8')
+    def readLineFile(self, fileDirect, fileName):
+        if not os.path.exists(fileDirect):
+           raise LookupError(fileDirect)
+        f = codecs.open(fileDirect + fileName, 'r', encoding='utf8')
         content = f.readline()
         f.close()
         return content
--- test_fileManager.py
+++ test_fileManager.py
@@ -3,45 +3,49 @@ import os
 import codecs
 from fileManager import FileManager
 
+
 class TestFileManager(TestCase):
 
     def test_writeFile(self):
         # given
-        nowDir = os.getcwd()
-        fileName = nowDir + '\\test\\testFileManager.txt'
+        nowDir = os.getcwd() + '\\test'
+        fileName = '\\testFileManager.txt'
+        fullFileName = nowDir + fileName
         content = "test"
 
         # when
         fileManager = FileManager()
-        fileManager.writeFile(fileName, content)
+        fileManager.writeFile(nowDir, fileName, content)
 
         # then
-        f = codecs.open(fileName, 'r', encoding='utf8')
+        f = codecs.open(fullFileName, 'r', encoding='utf8')
         resultContent = f.readline()
         f.close()
         self.assertEqual(resultContent, content)
 
         # test 후에 파일 삭제 
         # 파일 내용 실제로 보고 싶으면 주석
-        os.remove(fileName)
-        
+        print(fullFileName)
+        os.remove(fullFileName)
+
     def test_readLineFile(self):
         # given
-        nowDir = os.getcwd()
-        fileName = nowDir + '\\test\\testFileManager.txt'
+        nowDir = os.getcwd() + '\\test'
+        fileName = '\\testFileManager.txt'
+        fullFileName = nowDir + fileName
         content = "test"
 
-        f = codecs.open(fileName, 'w', encoding='utf8')
+        f = codecs.open(fullFileName, 'w', encoding='utf8')
         f.write(content)
         f.close()
 
         # when
         fileManager = FileManager()
-        resultContent = fileManager.readLineFile(fileName)
+        resultContent = fileManager.readLineFile(nowDir, fileName)
 
         # then
         self.assertEqual(resultContent, content)
 
         # test 후에 파일 삭제
         # 파일 내용 실제로 보고 싶으면 주석
-        os.remove(fileName)
+        os.remove(fullFileName)
